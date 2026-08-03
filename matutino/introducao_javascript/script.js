56197d2ee5aa20abe87997aaa76524c357e1fc2e
Rodrigo Bossini <professorbossini@gmail.com>
2026-08-03 09:09:26 -0300

    js(ap01): codigo 1.1.2
    continuar o exemplo com var

---
+++ matutino/introducao_javascript/script.js
@@ -0,0 +1,19 @@
+//const, let e var
+// var linguagem = "Javascript"
+// console.log('Aprendendo ' + linguagem)
+// var linguagem = 'Java'
+// console.log('Aprendendo ' + linguagem)
+// var nome = 'José'
+// console.log(nome)
+// nome = 'João'
+// console.log(nome)
+// let nome = 'José'
+// console.log(nome)
+// nome = 'João'
+// console.log(nome)
+
+// const nome = 'José'
+// console.log(nome)
+// nome = 'João'
+// const idade = 18
+// console.log(idade)
